462e6a3ce40abba4fc95249384c1d70d437d6a27
Jan Hurst <jan@janhurst.com>
2022-12-08 10:24:50 +0000

    test simple yeoman usage output

--- test/yeoman/test.sh
+++ test/yeoman/test.sh
@@ -27,7 +27,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "version" color | grep 'my favorite color is red'
+check "Usage: yo GENERATOR [args] [options]" yo --help | grep 'Usage: yo'
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
